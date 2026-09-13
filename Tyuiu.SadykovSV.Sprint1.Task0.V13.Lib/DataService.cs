c018f2c937a0c6ff734fe927fb6531c4d5e0a9c7
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-13 12:59:42 +0500

    Add project files.

---
+++ Tyuiu.SadykovSV.Sprint1.Task0.V13.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.SadykovSV.Sprint1.Task0.V13.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.SadykovSV.Sprint1.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SadykovSV.Sprint1.Task0.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
