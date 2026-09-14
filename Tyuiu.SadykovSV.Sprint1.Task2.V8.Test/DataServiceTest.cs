001a23f5299dbb97136bf9eaa02ebb1a7a0c58dd
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-14 23:16:11 +0500

    Task2

---
+++ Tyuiu.SadykovSV.Sprint1.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SadykovSV.Sprint1.Task2.V8.Lib;
+namespace Tyuiu.SadykovSV.Sprint1.Task2.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int a = 5;
+            int b = 10;
+            var result = ds.CalculatePerimetr(a, b);
+            Assert.AreEqual(30, result);
+        }
+    }
+}
--- Tyuiu.SadykovSV.Sprint1.Task2.V8/Program.cs
+++ Tyuiu.SadykovSV.Sprint1.Task2.V8/Program.cs
@@ -1,31 +1,30 @@
-﻿
-using Tyuiu.SadykovSV.Sprint1.Task1.V19.Lib;
-DataService ds = new DataService();
+﻿using Tyuiu.SadykovSV.Sprint1.Task2.V8.Lib;
+DataService ds= new DataService();
 Console.Title = "Спринт #1 | Выполнил: Садыков С.В. | ПИНБ-26-1";
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* Спринт #1                                                               *");
-Console.WriteLine("* Тема: Организация ввода и вывода данных в С#                            *");
-Console.WriteLine("* Задание #1                                                              *");
-Console.WriteLine("* Вариант #19                                                             *");
+Console.WriteLine("* Тема: Арифметические операторы в С#                                     *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #8                                                              *");
 Console.WriteLine("* Выполнил: Садыков С.В. | ПИНБ-26-1                                      *");
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                                                *");
 Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-Console.WriteLine("* вычисляет результат по формуле (x+10*y)/(x*y) и печатает его на экране. *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
 Console.WriteLine("*                                                                         *");
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* Исходные данные:                                                        *");
 Console.WriteLine("***************************************************************************");
-double x, y;
-Console.WriteLine("Введите значение Х: ");
-x= Convert.ToDouble(Console.ReadLine());
+int a, b;
+Console.WriteLine("Введите длину первой стороны прямоугольника: ");
+a = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите значение Y: ");
-y= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длину второй стороны прямоугольника: ");
+b = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
 Console.WriteLine("***************************************************************************");
 
-Console.WriteLine(ds.Calculate(x, y));
+Console.WriteLine("Периметр прямоугольника равен: " + ds.CalculatePerimetr(a, b));
 Console.ReadKey();
