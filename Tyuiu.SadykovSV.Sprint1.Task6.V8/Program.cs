b53b094d3b7f37c8268e138294a6b6ed50d16fc8
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-15 03:04:08 +0500

    Task6

--- Tyuiu.SadykovSV.Sprint1.Task6.V8/Program.cs
+++ Tyuiu.SadykovSV.Sprint1.Task6.V8/Program.cs
@@ -3,7 +3,7 @@ DataService ds = new DataService();
 Console.Title = "Спринт #1 | Выполнил: Садыков С.В. | ПИНБ-26-1";
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* Спринт #1                                                               *");
-Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+Console.WriteLine("* Тема: Работа со строками класс String                                   *");
 Console.WriteLine("* Задание #6                                                              *");
 Console.WriteLine("* Вариант #8                                                              *");
 Console.WriteLine("* Выполнил: Садыков С.В. | ПИНБ-26-1                                      *");
